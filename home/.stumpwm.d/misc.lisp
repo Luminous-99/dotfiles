e99a282adc195f749292ed709b229ecd8287df4a
Luminous-99 <ahmetakbash8@gmail.com>
2025-02-22 23:33:43 +0200

    shortened swank message

--- home/.stumpwm.d/misc.lisp
+++ home/.stumpwm.d/misc.lisp
@@ -27,7 +27,7 @@
     (swank:create-server :port 4005
                    :style swank:*communication-style*
                    :dont-close t)
-    (echo-string (current-screen) "Starting swank. M-x slime-connect RET RET, then (in-package stumpwm).")
+    (echo-string (current-screen) "Starting swank. Do \"M-x slime-connect.\"")
     (setf *swank-running* t)))
 
 (run-swank)
